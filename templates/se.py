a56a597c2b7159fef51d625240bd663d46ed4f7e
Pedro Roldan <pedro.roldang@mail.com>
2019-11-06 20:29:46 +0000

    .

--- templates/se.py
+++ templates/se.py
@@ -18,7 +18,7 @@ def get_points():
 
     path = Path(__file__).parent.parent.joinpath('20081026094426.csv')
     with open(path, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, fieldnames=("Latitude", "Longitude", "Nr", "Altitude", "DateFrom", "Date", "Time", "Distance", "Time in Sec.", "Vel. m/s", "Vel. km/h", "Mode"))
+        csv_reader = csv.DictReader(csv_file, fieldnames=("Latitude", "Longitude", "Nr", "Altitude", "DateFrom", "Date", "Time", "Distance (Km)", "Time (Sec)", "Vel. m/s", "Vel. km/h", "Mode"))
         line_count = 0
         for row in csv_reader:
             # if line_count == 0:
@@ -40,25 +40,25 @@ def get_points():
 
         pos = 0
         for row in serializedData:
-            if row["Time in Sec."] is None:
+            if row["Time (Sec)"] is None:
                 p1_timestamp = datetime.datetime.strptime(row["Date"] + ' ' + row["Time"], '%Y-%m-%d %H:%M:%S')
                 if pos + 1 <= len(serializedData) - 1:
                     next_row = serializedData[pos + 1]
                 if next_row is not None:
                     p2_timestamp = datetime.datetime.strptime(next_row["Date"] + ' ' + next_row["Time"], '%Y-%m-%d %H:%M:%S')
-                    row["Time in Sec."] = (p2_timestamp - p1_timestamp).total_seconds()
+                    row["Time (Sec)"] = (p2_timestamp - p1_timestamp).total_seconds()
 
-            if row["Distance"] is None:
+            if row["Distance (Km)"] is None:
                 p1 = (float(row["Latitude"]), float(row["Longitude"]))
                 if pos + 1 <= len(serializedData) - 1:
                     next_row = serializedData[pos + 1]
 
                 if next_row is not None:
                     p2 = (float(next_row["Latitude"]), float(next_row["Longitude"]))
-                    row["Distance"] = round(haversine(p1, p2), 2)
+                    row["Distance (Km)"] = round(haversine(p1, p2), 2)
             pos += 1
-            total_distance += row["Distance"]
-            total_time += row["Time in Sec."]
+            total_distance += row["Distance (Km)"]
+            total_time += row["Time (Sec)"]
 
     workbook = xlsxwriter.Workbook(XLSX_FILE)
     worksheet = workbook.add_worksheet('Data')
@@ -70,8 +70,8 @@ def get_points():
     worksheet.write('D1', 'Altitude')
     worksheet.write('E1', 'Date')
     worksheet.write('F1', 'Time')
-    worksheet.write('G1', 'Distance')
-    worksheet.write('H1', 'Time in Sec.')
+    worksheet.write('G1', 'Distance (Km)')
+    worksheet.write('H1', 'Time (Sec)')
 
     # lines
     line_number = 5
@@ -82,8 +82,8 @@ def get_points():
         worksheet.write(line_number, 3, row["Altitude"])
         worksheet.write(line_number, 4, row["Date"])
         worksheet.write(line_number, 5, row["Time"])
-        worksheet.write(line_number, 6, row["Distance"])
-        worksheet.write(line_number, 7, row["Time in Sec."])
+        worksheet.write(line_number, 6, row["Distance (Km)"])
+        worksheet.write(line_number, 7, row["Time (Sec)"])
         line_number += 1
 
     workbook.close()
